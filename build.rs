bcdfa62ba9eefcd59b4e75c97539ef8dc63fec11
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-01-14 22:57:33 +0100

    arch: rename to cpu

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
 fn main() {
     // Build startup code and archive it
     cc::Build::new()
-        .file("src/arch/startup.S")
+        .file("src/cpu/startup.S")
         .compile("libstartup.a");
 
     // Trigger a rerun on some external file changes
-    println!("cargo:rerun-if-changed=src/arch/startup.S");
-    println!("cargo:rerun-if-changed=src/arch/qemu/qemu-virt.ld");
+    println!("cargo:rerun-if-changed=src/cpu/startup.S");
+    println!("cargo:rerun-if-changed=src/cpu/qemu/qemu-virt.ld");
     println!("cargo:rerun-if-changed=build.rs");
 }
