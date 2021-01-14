af11b2a4e8d071ba40c24403d94f54daf8ccdaab
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-01-14 22:50:05 +0100

    asm: rename to arch

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
 fn main() {
     // Build startup code and archive it
     cc::Build::new()
-        .file("src/asm/startup.S")
+        .file("src/arch/startup.S")
         .compile("libstartup.a");
 
     // Trigger a rerun on some external file changes
-    println!("cargo:rerun-if-changed=src/asm/startup.S");
+    println!("cargo:rerun-if-changed=src/arch/startup.S");
     println!("cargo:rerun-if-changed=src/scripts/qemu-virt.ld");
     println!("cargo:rerun-if-changed=build.rs");
 }
