deea70e4948e4ffe186476db3da17fd2d46b86fa
Pietro Lorefice <pietro@develer.com>
2021-01-17 17:03:34 +0100

    cpu: pass parameters to exception handler

--- src/cpu/trap.rs
+++ src/cpu/trap.rs
@@ -2,42 +2,28 @@ use super::*;
 
 extern "C" {
     // Defined in trap.S
-    fn _strap_entry();
+    fn trap_entry();
 }
 
 #[no_mangle]
 #[link_section = ".trap.rust"]
-#[export_name = "_do_handle_strap_rust"]
-pub extern "C" fn do_handle_strap_rust() {
-    let cause = SCAUSE.read();
-
+pub extern "C" fn handle_exception(cause: usize, epc: usize, tval: usize, _regp: usize) -> usize {
     println!(
-        r#"
-=> Trap handler!
-    sstatus: {:016x}
-     scause: {:016x}
-      stval: {:016x}
-        sip: {:016x}
-        sie: {:016x}
-       sepc: {:016x}
-"#,
-        SSTATUS.read(),
-        cause,
-        STVAL.read(),
-        SIP.read(),
-        SIE.read(),
-        SEPC.read()
+        "=> Exception: cause {:016x}, epc {:016x}, tval {:016x}",
+        cause, epc, tval,
     );
 
     // Breakpoint: skip the EBREAK instruction
     if cause == 0x3 {
-        SEPC.modify(|pc| pc + 4);
+        epc + 4
+    } else {
+        epc
     }
 }
 
 /// Configures the trap vector used to handle traps in S-mode.
 pub fn init_trap_vector() {
-    STVEC.write(_strap_entry as *const () as usize);
+    STVEC.write(trap_entry as *const () as usize);
 
     // Enable interrupts
     SIE.modify(|r| r | (1 << 9) | (1 << 5) | (1 << 1));
