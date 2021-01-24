c5c64d3f95aaf15c7e12d1606400a22e5142c7b9
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-01-24 19:29:21 +0100

    kernel/drivers: fix syscon documentation

--- kernel/src/drivers/syscon.rs
+++ kernel/src/drivers/syscon.rs
@@ -4,11 +4,11 @@ use spin::Mutex;
 use crate::mm::mmio::RW;
 
 lazy_static! {
-    /// Instance of the UART0 serial port on this machine.
+    /// Instance of the system controller on this machine.
     pub static ref SYSCON: Mutex<Syscon> = Mutex::new(Syscon::new(0x0010_0000));
 }
 
-/// Device driver of the SYSCON peripheral.
+/// Device driver of the system controller peripheral.
 pub struct Syscon {
     p: &'static mut RegisterBlock,
 }
@@ -19,19 +19,19 @@ struct RegisterBlock {
 }
 
 impl Syscon {
-    /// Creates a new SYSCON mapping at the given address.
+    /// Creates a new system controller mapped at the given address.
     pub const fn new(addr: usize) -> Self {
         Self {
             p: unsafe { &mut *(addr as *mut RegisterBlock) },
         }
     }
 
-    /// Sends a shutdown signal to the SYSCON with the given exit code.
+    /// Sends a shutdown signal to the system controller with the given exit code.
     pub fn poweroff(&mut self, code: u32) {
         unsafe { self.p.reg.write((code << 16) | 0x3333) };
     }
 
-    /// Sends a reboot signal to the SYSCON.
+    /// Sends a reboot signal to the system controller.
     pub fn reboot(&mut self) {
         unsafe { self.p.reg.write(0x7777) };
     }
