c848e077f8291f180c5a0db9f9c4fa90a23a66fd
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-01-25 19:44:20 +0100

    kernel/riscv: print SBI extensions

--- kernel/src/arch/riscv/sbi.rs
+++ kernel/src/arch/riscv/sbi.rs
@@ -15,9 +15,23 @@ pub fn init() {
             Base::get_impl_id().unwrap(),
             Base::get_impl_version().unwrap(),
         );
+        kprint!("SBI v0.2 detected extensions:");
         if Base::probe_extension(Extension::Timer).is_ok() {
-            kprintln!("SBI v0.2 TIME extension detected");
+            kprint!(" TIMER");
         }
+        if Base::probe_extension(Extension::Ipi).is_ok() {
+            kprint!(" IPI");
+        }
+        if Base::probe_extension(Extension::Rfence).is_ok() {
+            kprint!(" RFENCE");
+        }
+        if Base::probe_extension(Extension::Hsm).is_ok() {
+            kprint!(" HSM");
+        }
+        if Base::probe_extension(Extension::SystemReset).is_ok() {
+            kprint!(" SYSRST");
+        }
+        kprintln!();
     } else {
         panic!("Unsupported SBI specification");
     }
