ef046185e4477f952a91ecb5a6ef134ecc6404f9
Pietro Lorefice <pietro@develer.com>
2021-01-26 08:09:29 +0100

    kernel/riscv: improve readability

--- kernel/src/arch/riscv/time.rs
+++ kernel/src/arch/riscv/time.rs
@@ -1,4 +1,4 @@
-use crate::mm::mmio::read_volatile;
+use crate::mm::mmio;
 
 const CLINT_BASE: usize = 0x0200_0000;
 const CLINT_TIME_OFFSET: usize = 0xbff8;
@@ -8,7 +8,7 @@ pub const CLINT_TIMEBASE: u64 = 10_000_000;
 
 /// Returns the number of cycles elapsed since boot, in timebase units.
 pub fn get_cycles() -> u64 {
-    read_volatile(CLINT_BASE, CLINT_TIME_OFFSET)
+    mmio::read_volatile(CLINT_BASE, CLINT_TIME_OFFSET)
 }
 
 /// Schedules a timer interrupt to happend `interval` ticks in the future.
